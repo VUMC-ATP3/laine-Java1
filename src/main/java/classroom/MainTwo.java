641487f15f6ffa13b5d307f333c8a4884b9954a1
Vilnis <82605551+LDreismane@users.noreply.github.com>
2022-05-10 20:53:13 +0200

    Add extra examples

---
+++ src/main/java/classroom/MainTwo.java
@@ -0,0 +1,18 @@
+package classroom;
+
+public class MainTwo {
+    public static void main(String[] args) {
+        String vards = "Laine";
+        String uzvards = "Dreismane";
+        int vecums = 36;
+        boolean isPresent = true;
+
+        System.out.println("Vārds: " + vards + "\n" + "Uzvārds " + uzvards + "\n" + "Vecums" + vecums + "\n"
+        + "Vai bija nodarbībā? " + isPresent);
+
+        //%s vards tiks ielikts tajā %s laukā
+        String teikums = String.format("Vārds: %s \n Uzvārds %s \n Vecums %s \n isPresent %s",vards,uzvards,vecums,isPresent);
+        System.out.println(teikums);
+
+    }
+}
